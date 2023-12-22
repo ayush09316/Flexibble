94b113c5eb0d23166b96c8fcada8c6a795ef6275
Ayush Sharma <ayushsharma170199@gmail.com>
2023-12-22 17:18:36 +0530

    grafbase correction

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -6,7 +6,7 @@ const User= g.model('User',{
   avatarUrl:g.url(),
   description:g.string().optional(),
   githubUrl:g.url().optional(),
-  linkedinUrl:g.url().optional(),
+  linkedInUrl:g.url().optional(),
   projects:g.relation(()=>Project).list().optional(),
 }).auth((rules)=>{
   rules.public().read()
