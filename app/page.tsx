a807f0e0a2f7f9cb6da5bb84a72895518637f402
Ayush Sharma <ayushsharma170199@gmail.com>
2023-12-22 17:35:57 +0530

    typescript correction

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-import { ProjectInterface } from "@/common.types";
-import Categories from "@/components/Categories";
 import LoadMore from "@/components/LoadMore";
-import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
-
-type SearchParams = {
-  category?: string | null;
-  endcursor?: string | null;
-}
-
-type Props = {
-  searchParams: SearchParams
-}
+import Categories from "@/components/Categories";
+import { ProjectInterface } from "@/common.types";
+import ProjectCard from "@/components/ProjectCard";
 
 type ProjectSearch = {
   projectSearch: {
@@ -25,16 +16,25 @@ type ProjectSearch = {
   },
 }
 
-export const dynamic = 'force-dynamic';
+type SearchParams = {
+  category?: string;
+  endcursor?: string;
+} 
+
+type Props = {
+  searchParams: SearchParams;
+}
+
+export const dynamic = 'force-dynamic'; // force dynamic for this page so that it is always regenerated when a new project is created
 export const dynamicParams = true;
 export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
-  const data = await fetchAllProjects(category, endcursor) as ProjectSearch
+  const data = await fetchAllProjects(category, endcursor) as ProjectSearch;
 
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
-  if (projectsToDisplay.length === 0) {
+  if(projectsToDisplay.length === 0) {
     return (
       <section className="flexStart flex-col paddings">
         <Categories />
@@ -44,32 +44,34 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
     )
   }
 
+  const pagination = data?.projectSearch?.pageInfo;
+
   return (
-    <section className="flexStart flex-col paddings mb-16">
-      <Categories />
+    <section className="flex-start flex-col paddings mb-16">
+        <Categories />
 
-      <section className="projects-grid">
-        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
-          <ProjectCard
-            key={`${node?.id}`}
-            id={node?.id}
-            image={node?.image}
-            title={node?.title}
-            name={node?.createdBy.name}
-            avatarUrl={node?.createdBy.avatarUrl}
-            userId={node?.createdBy.id}
-          />
-        ))}
-      </section>
+        <section className="projects-grid">
+          {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+            <ProjectCard 
+              key={node?.id}
+              id={node?.id}
+              image={node?.image}
+              title={node?.title}
+              name={node?.createdBy?.name}
+              avatarUrl={node?.createdBy?.avatarUrl}
+              userId={node?.createdBy?.id}
+            />
+          ))}
+        </section>
 
-      <LoadMore 
-        startCursor={data?.projectSearch?.pageInfo?.startCursor} 
-        endCursor={data?.projectSearch?.pageInfo?.endCursor} 
-        hasPreviousPage={data?.projectSearch?.pageInfo?.hasPreviousPage} 
-        hasNextPage={data?.projectSearch?.pageInfo.hasNextPage}
-      />
+        <LoadMore 
+          startCursor={pagination.startCursor}
+          endCursor={pagination.endCursor}
+          hasPreviousPage={pagination.hasPreviousPage}
+          hasNextPage={pagination.hasNextPage}
+        />
     </section>
   )
-};
+}
 
-export default Home;
+export default Home
